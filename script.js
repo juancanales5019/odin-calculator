d66b18da70380f5d25bb0675b6e96a919b49c7dc
Juan Pineda <juancanales5019@gmail.com>
2024-11-12 22:15:15 -0600

    Add pseudo code to the functionality
    this will be the base for the real implementation

---
+++ script.js
@@ -0,0 +1,15 @@
+let operation = document.querySelector(".operation");
+let result = document.querySelector(".result");
+
+//obtener primer numero y desplegarlo en operation
+//obtener operador y desplegarlo en operation
+//obtener el segundo numero y desplegarlo en operation
+//evaluar la operacion para determinar de que tipo es...
+//realizar la operacion y desplegar el resultado en result
+
+//si se quiere hacer una operacion ademas de la primera
+//conservar result como valor del primer numero y desplegarlo en operation
+//obtener operador y desplegarlo en operation
+//obtener el segundo numero y desplegarlo en operation
+//evaluar la operacion para determinar de que tipo es...
+//realizar la operacion y desplegar el resultado en result
